53f8fbcfe331511ccf8f57daa712ef29ecb4d682
nazmulmilon <milonsarker73@gmail.com>
2022-02-07 15:25:17 +0600

    get method for API

--- employee/urls.py
+++ employee/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url
+#from django.conf.urls import url
 
 from hrm.api import UserList
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^api/user_list/$', UserList.as_view(), name='user_list'),
+    path('api/user_list/', UserList.as_view(), name='user_list'),
+
+    #url(r'^api/user_list/$', UserList.as_view(), name='user_list'),
 
 ]
--- hrm/serializers.py
+++ hrm/serializers.py
@@ -5,5 +5,5 @@ from hrm.models import Users
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Users
-        fields = ('name', 'employee_id')
-        # fields = '__all__'
+        # fields = ('name', 'employee_id')
+        fields = '__all__'
