61aaec7180e87160bc220aefa8355bb348a241ba
nazmulmilon <milonsarker73@gmail.com>
2022-02-07 20:28:07 +0600

    Delete API and API authentication done

--- hrm/api.py
+++ hrm/api.py
@@ -19,21 +19,29 @@ class UserList(APIView):
 
 
 class UserDetail(APIView):
-    def get(self, request, employee_id):
+    def get_user(self, employee_id):
         try:
             model = Users.objects.get(id=employee_id)
+            return model
         except Users.DoesNotExist:
+            return
+
+    def get(self, request, employee_id):
+        if not self.get_user(employee_id):
             return Response(f'User with {employee_id} is  Not Found in database', status=status.HTTP_404_NOT_FOUND)
-        serializer = UserSerializer(model)
+        serializer = UserSerializer(self.get_user(employee_id))
         return Response(serializer.data)
 
     def put(self, request, employee_id):
-        try:
-            model = Users.objects.get(id=employee_id)
-        except Users.DoesNotExist:
-            return Response(f'User with {employee_id} is  Not Found in database', status=status.HTTP_404_NOT_FOUND)
-        serializer = UserSerializer(model, data=request.data)
+        serializer = UserSerializer(self.get_user(employee_id), data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, employee_id):
+        if not self.get_user(employee_id):
+            return Response(f'User with {employee_id} is  Not Found in database', status=status.HTTP_404_NOT_FOUND)
+        model = self.get_user(employee_id)
+        model.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
